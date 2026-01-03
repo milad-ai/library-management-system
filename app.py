40336875b66cde345d575f451cfab41edf092ac4
Milad Vazan <165850937+milad-ai@users.noreply.github.com>
2026-01-03 15:05:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -421,4 +421,4 @@ def page_500():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
